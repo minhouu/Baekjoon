7b42d69ad6e0a0092e8f9e1a9870ed2753c5f4df
Minho Choi <109497339+minhouu@users.noreply.github.com>
2022-07-20 09:48:51 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.py
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.py
@@ -1,10 +1,4 @@
 data = list(map(int, input().split()))
 
-def money_share(m: int, n: int) -> int :
-    return m//n
-
-def money_remainder(m: int, n: int) -> int :
-    return m%n
-    
-print(money_share(data[0],data[1]))
-print(money_remainder(data[0],data[1]))
+print(data[0]//data[1])
+print(data[0]%data[1])
