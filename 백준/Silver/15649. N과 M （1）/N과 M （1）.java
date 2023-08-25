eedcf57d22b2728e6ae5488641bde6928fce8b14
CHOE Minho <tortor1403@gmail.com>
2023-08-25 13:45:26 +0900

    [Silver III] Title: N과 M (1), Time: 2144 ms, Memory: 62104 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
 	static int N;
 	static int M;
+	static int[] visited;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		M = sc.nextInt();
-		help(new int[M],  0);
+		visited = new int[N + 1];
+		help(new int[M + 1], 0);
 		sc.close();
 
 	}
@@ -21,22 +23,16 @@ public class Main {
 			System.out.println();
 			return;
 		}
-		
+
 		for (int i = 1; i <= N; i++) {
-			boolean isUsed = false;
-			for (int j = 0; j <= len; j++) {
-				if (result[j] == i) {
-					isUsed = true;
-					break;
-				}
-			}
-			if (isUsed) {
-				continue;
+			if (visited[i] != 1) {
+				result[len] = i;
+				visited[i] = 1;
+				help(result, len + 1);
+				visited[i] = 0;
 			}
 //			System.out.println("포문" + len + " " + i);
-			result[len] = i;
-			help(result, len + 1);
-			result[len] = 0;
+			
 		}
 	}
 }
